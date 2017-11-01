ae62c431c7dcaed20b5212c3f79c859bae592e5e
Serg Kalinin <kalinin.s@digdes.com>
2017-11-01 21:10:38 +0300

    intial commit

---
+++ Eaterias/EateriesTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  EateriesTableViewCell.swift
+//  Eaterias
+//
+//  Created by Serg Kalinin on 01/11/2017.
+//  Copyright © 2017 Serg Kalinin. All rights reserved.
+//
+
+import UIKit
+
+class EateriesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

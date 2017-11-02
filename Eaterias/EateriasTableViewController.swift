e64da8b7cce9503c2e57ed74eafbd5d57d1281dc
Serg Kalinin <kalinin.s@digdes.com>
2017-11-02 20:20:34 +0300

    13 lesson

--- Eaterias/EateriasTableViewController.swift
+++ Eaterias/EateriasTableViewController.swift
@@ -52,49 +52,31 @@ class EateriasTableViewController: UITableViewController {
     }
     
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        let ac = UIAlertController(title: nil, message:"Выберете действие" , preferredStyle: .actionSheet)
+        let call = UIAlertAction(title:"позвонить 7999214140\(indexPath.row)", style:.default){
+            (action:UIAlertAction) -> Void in
+            let alertC = UIAlertController(title: nil, message:"Вызов не может быть совершен" , preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertC.addAction(ok)
+            self.present(alertC, animated: true, completion: nil)
+            
+        }
+        let isVisited = UIAlertAction(title: "Я здесь был", style: .default){
+            (action) in
+            let cell = tableView.cellForRow(at: indexPath)
+            cell?.accessoryType = .checkmark
+            
+        }
+        let cancel = UIAlertAction(title:"Отмена", style:.cancel, handler:nil)
+        ac.addAction(cancel)
+        ac.addAction(isVisited)
+        ac.addAction(call)
+        present(ac, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    
 
 }

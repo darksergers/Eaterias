9538b9ae3e6591a7f26d7410f3e903b00b098475
Serg Kalinin <kalinin.s@digdes.com>
2018-03-19 21:52:10 +0300

    add CoreData. 39 Lesson

--- Eateries/AppDelegate.swift
+++ Eateries/AppDelegate.swift
@@ -11,7 +11,11 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window: UIWindow?
+    var window: UIWindow?
+    
+    lazy var coreDataStack = CoreDataStack()
+
+    
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -45,7 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 
   func applicationWillTerminate(_ application: UIApplication) {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    self.coreDataStack.saveContext()
   }
 
 
--- Eateries/MapViewController.swift
+++ Eateries/MapViewController.swift
@@ -20,7 +20,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
       mapView.delegate = self
       
       let geocoder = CLGeocoder()
-      geocoder.geocodeAddressString(restaurant.location) { (placemarks, error) in
+      geocoder.geocodeAddressString(restaurant.location!) { (placemarks, error) in
         
         guard error == nil else { return }
         guard let placemarks = placemarks else { return }
@@ -54,7 +54,7 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     }
     
     let rightImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
-    rightImage.image = UIImage(named: restaurant.image)
+    rightImage.image = UIImage(data:restaurant.image! as Data)
     annotationView?.rightCalloutAccessoryView = rightImage
     
     annotationView?.pinTintColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
